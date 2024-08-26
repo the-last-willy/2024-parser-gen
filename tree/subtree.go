6ebdaeff13e3e8edf7833b45c2249bfecb2eec3d
Willy Jacquet <willy.jacquet@outlook.com>
2024-08-26 20:12:09 +0200

    Add subtree

---
+++ tree/subtree.go
@@ -0,0 +1,31 @@
+package tree
+
+type SubTree[Data any] struct {
+	tree Tree[Data]
+	root Node
+}
+
+func NewSubTree[Data any](t Tree[Data], root Node) SubTree[Data] {
+	return SubTree[Data]{
+		tree: t,
+		root: root,
+	}
+}
+
+func (st SubTree[Data]) WithRoot(root Node) SubTree[Data] {
+	return NewSubTree(st.tree, root)
+}
+
+// Tree interface
+
+func (st SubTree[Data]) ChildrenOf(n Node) []Node {
+	return st.tree.ChildrenOf(n)
+}
+
+func (st SubTree[Data]) DataOf(n Node) Data {
+	return st.tree.DataOf(n)
+}
+
+func (st SubTree[Data]) Root() Node {
+	return st.root
+}
