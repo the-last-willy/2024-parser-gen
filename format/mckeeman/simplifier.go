c65be4e8fe29e9c911df06c93ed690b5c3314d4c
Willy Jacquet <willy.jacquet@outlook.com>
2024-08-26 19:24:34 +0200

    Remove dead code

--- format/mckeeman/simplifier.go
+++ format/mckeeman/simplifier.go
@@ -6,69 +6,6 @@ import (
 	"slices"
 )
 
-func Simplify(tree parse.DataTree) parse.DataTree {
-	return tree.WithRoot(simplify(tree)[0])
-}
-
-func simplify(tree parse.DataTree) []*parse.Tree {
-	rootCopy := *tree.Root
-
-	if rootCopy.Type == "indentation" {
-		rootCopy.Children = []*parse.Tree{}
-		return []*parse.Tree{}
-	}
-	if rootCopy.Type == "newline" {
-		rootCopy.Children = []*parse.Tree{}
-		return []*parse.Tree{}
-	}
-	if rootCopy.Type == "space" {
-		rootCopy.Children = []*parse.Tree{}
-		return []*parse.Tree{}
-	}
-
-	if rootCopy.Type == "name" {
-		rootCopy.Children = []*parse.Tree{}
-		return []*parse.Tree{&rootCopy}
-	}
-
-	simplifiedChildren := []*parse.Tree{}
-	for _, child := range tree.Root.Children {
-		simplifiedChildren = append(simplifiedChildren, simplify(tree.WithRoot(child))...)
-	}
-
-	if rootCopy.Type == "alternatives" {
-		rootCopy.Children = []*parse.Tree{}
-		return simplifiedChildren
-	}
-	if rootCopy.Type == "element" {
-		rootCopy.Children = []*parse.Tree{}
-		return simplifiedChildren
-	}
-	if rootCopy.Type == "elements" {
-		rootCopy.Children = []*parse.Tree{}
-		return simplifiedChildren
-	}
-	if rootCopy.Type == "items" {
-		rootCopy.Children = []*parse.Tree{}
-		return simplifiedChildren
-	}
-	if rootCopy.Type == "literal" {
-		rootCopy.Children = []*parse.Tree{}
-		return simplifiedChildren
-	}
-	if rootCopy.Type == "rules" {
-		rootCopy.Children = []*parse.Tree{}
-		return simplifiedChildren
-	}
-	if rootCopy.Type == "singleton" {
-		rootCopy.Children = []*parse.Tree{}
-		return simplifiedChildren
-	}
-
-	rootCopy.Children = simplifiedChildren
-	return []*parse.Tree{&rootCopy}
-}
-
 type Simplifier struct {
 	derecursifier *parse.Derecursifier
 	pruner        *tree.Pruner[parse.TreeData]
--- parse/tree.go
+++ parse/tree.go
@@ -1,9 +1,7 @@
 package parse
 
 import (
-	"fmt"
 	"parsium/tree"
-	"strings"
 )
 
 type TreeData struct {
@@ -38,100 +36,4 @@ func FindAllWithType(t tree.Tree[TreeData], ty string) []tree.Node {
 
 func FindFirstWithType(t tree.Tree[TreeData], ty string) *tree.Node {
 	return tree.FindFirst(t, HasTypePred(ty))
-}
-
-type Tree struct {
-	Type string
-
-	First int
-	Last  int
-
-	Data string
-
-	Children []*Tree
-}
-
-func (t *Tree) Derecursified() *Tree {
-	children := []*Tree{}
-	for _, child := range t.Children {
-		child := child.Derecursified()
-		if child.Type == t.Type {
-			children = append(children, child.Children...)
-		} else {
-			children = append(children, child)
-		}
-	}
-
-	cp := *t
-	cp.Children = children
-	return &cp
-}
-
-func (t *Tree) FindAll(ty string) []*Tree {
-	found := []*Tree{}
-	t.TraverseAll(func(node *Tree) {
-		if node.Type == ty {
-			found = append(found, node)
-		}
-	})
-	return found
-}
-
-func (t *Tree) FindFirst(ty string) *Tree {
-	var found *Tree
-	t.TraverseAll(func(node *Tree) {
-		if found == nil && node.Type == ty {
-			found = node
-		}
-	})
-	return found
-}
-
-func (t *Tree) GetData(store string) string {
-	if store == "" {
-		return t.Data
-	}
-	if t.Data == "" {
-		return store[t.First:t.Last]
-	}
-	if store[t.First:t.Last] != t.Data {
-		panic("stored data are different")
-	}
-	return t.Data
-}
-
-func (t *Tree) PrintIndent(store string, indent int) {
-	id := strings.Repeat("  ", indent)
-	data := ""
-	if len(t.Children) == 0 {
-		data = store[t.First:t.Last]
-		data = strings.ReplaceAll(data, "\n", `\n`)
-		//data = strings.ReplaceAll(data, `"`, `\"`)
-		data = ` = '` + data + `'`
-	}
-	fmt.Printf("%s(%d, %d) %s%s\n", id, t.First, t.Last, t.Type, data)
-	for _, child := range t.Children {
-		child.PrintIndent(store, indent+1)
-	}
-}
-
-func (t *Tree) TraverseAll(do func(*Tree)) {
-	do(t)
-	for _, child := range t.Children {
-		child.TraverseAll(do)
-	}
-}
-
-func (t *Tree) ToTreeNode(tr tree.Tree[TreeData]) tree.Node {
-	children := []tree.Node{}
-	for _, child := range t.Children {
-		children = append(children, child.ToTreeNode(tr))
-	}
-	return tr.NewNode(
-		TreeData{
-			Type:  t.Type,
-			First: t.First,
-			Last:  t.Last,
-		},
-		children)
 }
