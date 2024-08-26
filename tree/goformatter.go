94276752397e1836b36491abc2e4c2f74818e085
Willy Jacquet <willy.jacquet@outlook.com>
2024-08-26 20:12:36 +0200

    Refact GoFormatter with SubTree

--- tree/goformatter.go
+++ tree/goformatter.go
@@ -7,37 +7,37 @@ type GoFormatter[Data any] struct {
 }
 
 func (f GoFormatter[Data]) Format(t Tree[Data]) string {
-	return f.format(t, 0)
+	return f.format(NewSubTree(t, t.Root()), 0)
 }
 
-func (f GoFormatter[Data]) format(t Tree[Data], indent int) string {
+func (f GoFormatter[Data]) format(st SubTree[Data], indent int) string {
 	s := fmt.Sprintf(
-		`t.NewNode(
+		`st.NewNode(
 %s,
     []tree.Node{
 %s
     },
 )`,
-		f.formatData(t, indent),
-		f.formatChildren(t, indent),
+		f.formatData(st),
+		f.formatChildren(st),
 	)
 	return IndentStr(s, indent)
 }
 
-func (f GoFormatter[Data]) formatData(t Tree[Data], indent int) string {
-	r := t.Root()
-	d := t.DataOf(r)
+func (f GoFormatter[Data]) formatData(st SubTree[Data]) string {
+	r := st.Root()
+	d := st.DataOf(r)
 	s := f.FormatData(d)
 	return IndentStr(s, 1)
 }
 
-func (f GoFormatter[Data]) formatChildren(t Tree[Data], indent int) string {
+func (f GoFormatter[Data]) formatChildren(st SubTree[Data]) string {
 	s := ""
-	for i, c := range t.ChildrenOf(t.Root()) {
+	for i, c := range st.ChildrenOf(st.Root()) {
 		if i != 0 {
 			s += "\n"
 		}
-		s += f.format(t.WithRoot(c), 2) + ","
+		s += f.format(st.WithRoot(c), 2) + ","
 	}
 	return s
 }
